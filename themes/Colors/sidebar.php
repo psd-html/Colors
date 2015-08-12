a993a2dfcbb1967df66953a45e069a1c7427a7e9
psd-html <contact@psd-html.fr>
2015-08-12 18:53:10 +0200

    MàJ sidebar

--- themes/Colors/sidebar.php
+++ themes/Colors/sidebar.php
@@ -3,18 +3,10 @@
 <aside class="bloc-4">
          
     <div class="widget">
-        <h3>Tester les thèmes</h3>
-         <?php eval($plxShow->callHook('MySkinSelect')) ?>
-    </div> 
-
-    <div class="widget">
-        <h3>Plugin jQMeter</h3>
-        <?php eval($plxShow->callHook('jQMeter')) ?> 
-    </div>    
-          
-    <div class="widget">
-        <h3><i class="flaticon-github2"></i> Plugin GitHub</h3>
-        <?php eval($plxShow->callHook('github')) ?> 
+        <h3><?php $plxShow->lang('LATEST_ARTICLES'); ?></h3>
+        <ul>
+           <?php $plxShow->lastArtList('<li><a class="#art_status" href="#art_url" title="#art_title">#art_title</a></li>'); ?>
+        </ul> 
     </div>    
 
     <div class="widget">
@@ -25,17 +17,30 @@
         </ul>
     </div>
     
-     
+     <div class="widget">
+        <!-- liste des Tags -->
+        <h3><?php $plxShow->lang('TAGS'); ?></h3>
+        <ul>
+           <?php $plxShow->tagList('<li><a class="tag" href="#tag_url" title="#tag_name">#tag_name</a></li>', 20); ?>
+        </ul>
+    </div>
+
     <div class="cb"></div>
      
      <div class="widget">
-         <h3><?php $plxShow->lang('LATEST_ARTICLES'); ?></h3>
-         <ul>
-           <?php $plxShow->lastArtList('<li><a class="#art_status" href="#art_url" title="#art_title">#art_title</a></li>'); ?>
-       </ul>
-     </div>
+        <!-- les dernieres commentaires -->
+        <h3><?php $plxShow->lang('LATEST_COMMENTS'); ?></h3>
+        <ul>
+           <?php $plxShow->lastComList('<li class="com"><a href="#com_url"><span>#com_author '.$plxShow->getLang('SAID').' :</span> #com_content(34)</a></li>'); ?>
+        </ul>
+    </div>
+    
+    <div class="widget">
+        <!-- les archives -->
+        <h3><?php $plxShow->lang('ARCHIVES'); ?></h3>
+        <ul>
+           <?php $plxShow->archList('<li><a class="#archives_status" href="#archives_url" title="#archives_name">#archives_name</a> (#archives_nbart)</li>'); ?>
+        </ul>
+    </div>
 
-
-     
-     
 </aside>
